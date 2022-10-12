c403a4988a2dff78a6ace1950d79a9d9dcb3b212
Carter Peene <carter.peene@alleninstitute.org>
2022-10-12 15:33:27 -0700

    Another test change. This time it has an accompanying .py file

---
+++ docs/visualization/visualization_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Visualizing an NWB File
+
+# ## Hello World
+
+# In[1]:
+
+
+print("Hello world!")
+
+
+# In[1]:
+
+
+print("Hello World again...")
+
+
+# In[ ]:
+
+
+
+
